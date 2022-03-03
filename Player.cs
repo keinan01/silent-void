5edf249a985b147875ae5c74177fa39c69537321
keinan01 <95875928+keinan01@users.noreply.github.com>
2022-03-03 12:08:28 -0600

    hai

---
+++ Player.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Silent_Void
+
+{
+    class Player : Entity
+    {
+        public Vector2 cameraPos;
+        int cooldown = 0;
+        int reload = 10;
+        public int points;
+        float acc = 1f;
+        int speed = 8;
+        public Player(Texture2D tex, Vector2 pos, float rad)
+        {
+            base.colour = Color.White;
+            base.tex = tex;
+            size = new Vector2(72, 72);
+            base.pos = pos;
+            base.rad = rad;
+            friendly = true;
+            isBullet = false;
+            points = 0;
+            vel = new Vector2(0, 0);
+            
+        }
+        public override void Update()
+        {
+            MouseState mouseState = Mouse.GetState();
+
+            Vector2 mousePosition = new Vector2(mouseState.X, mouseState.Y) - cameraPos;
+
+            Vector2 dPos = this.pos - mousePosition;
+
+            rad = (float)Math.Atan2(dPos.Y, dPos.X);
+
+            //vel = -new Vector2((float)Math.Cos(rad), (float)Math.Sin(rad)) * 5;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            
+            {
+                vel.Y -= acc;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+
+            {
+                vel.X -= acc;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+
+            {
+                vel.Y += acc;
+            }
+
+            if(vel.LengthSquared() > speed*speed)
+            {
+                vel.Normalize();
+                vel *= speed;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+
+            {
+                vel.X += acc;
+            }
+
+            if (pos.X < 0)
+            {
+                pos.X = 0;
+                vel.X = -vel.X;
+            }
+            if (pos.X > 1920)
+            {
+                pos.X = 1920;
+                vel.X = -vel.X;
+            }
+            if (pos.Y < 0)
+            {
+                pos.Y = 0;
+                vel.Y = -vel.Y;
+            }
+            if (pos.Y > 1080)
+            {
+                pos.Y = 1080;
+                vel.Y = -vel.Y;
+            }
+
+            if (mouseState.LeftButton == ButtonState.Pressed && cooldown >= reload)
+            {
+                Shoot(pos, rad, true, vel);
+                cooldown = 0;
+            }
+            cooldown++;
+
+            base.Update();
+        }
+
+        public void Shoot(Vector2 pos, float rad, bool friendly, Vector2 vel)
+        {
+            game.Add(new Projectile(game.bullet, pos, rad, friendly, vel));
+        }
+
+
+    }
+}
---
+++ Projectile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Silent_Void
+{
+    class Projectile : Entity
+    {
+
+        public Projectile(Texture2D tex, Vector2 pos, float rad, bool friendly, Vector2 vel)
+        {
+            base.colour = Color.White;
+            base.tex = tex;
+            size = new Vector2(25, 25);
+            base.pos = pos;
+            base.rad = rad;
+            base.friendly = friendly;
+            isBullet = true;
+
+            this.vel = vel -new Vector2((float)Math.Cos(rad), (float)Math.Sin(rad)) * 15;
+        }
+        public override void Update()
+        {
+
+            if ((player.pos - pos).Length() > 2000)
+            {
+                removed = true;
+            }
+
+            base.Update();
+        }
+    }
+}
