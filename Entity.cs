d9f6c30fd8266f31e808d7f017c6265d58103149
yugi20015 <71729728+yugi20015@users.noreply.github.com>
2022-04-11 11:57:42 -0500

    Update Entity.cs
    comments

--- Entity.cs
+++ Entity.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework;
@@ -14,6 +14,7 @@ namespace Silent_Void
 {
     public class Entity
     {
+        // instance fields like position, velocity, colour, etc for managing the state of the entity
         public Vector2 pos, vel, size, hitBoxSize;
         public Texture2D tex;
         public Color colour;
@@ -26,23 +27,23 @@ namespace Silent_Void
         public bool isBullet;
         public int hp, maxHp;
         public bool invincible = false;
-        public Entity()
+        public Entity() // entity constructor
         {
             hitBoxSize = new Vector2(0, 0);
         }
 
-        public void Draw(SpriteBatch spriteBatch, Vector2 shift)
+        public void Draw(SpriteBatch spriteBatch, Vector2 shift) // draw the enemy with variables that update on movement
         {
             Vector2 origin = new Vector2(tex.Bounds.Width / 2, tex.Bounds.Height / 2);
             spriteBatch.Draw(tex, new Rectangle((int)(pos.X + shift.X), (int)(pos.Y + shift.Y), (int)size.X, (int)size.Y), null, colour, rad - (float)Math.PI / 2, origin, SpriteEffects.None, 0f);
         }
 
-        public virtual void Update()
+        public virtual void Update() // move it generically
         {
             pos += vel;
         }
 
-        public virtual void OnHit()
+        public virtual void OnHit() // remove hp - mainly for player
         {
             hp--;
             if (hp <= 0)
@@ -52,15 +53,15 @@ namespace Silent_Void
         }
 
 
-        public virtual void OnDeath()
+        public virtual void OnDeath() 
         {
         }
 
-        public bool collides(Entity other)
+        public bool collides(Entity other) // returns boolean value upon collision
         {
             return new Rectangle((int)(pos.X - hitBoxSize.X / 2), (int)(pos.Y - hitBoxSize.Y / 2), (int)hitBoxSize.X, (int)hitBoxSize.Y - 20).Intersects(new Rectangle((int)(other.pos.X - other.hitBoxSize.X / 2), (int)(other.pos.Y - other.hitBoxSize.Y / 2), (int)other.hitBoxSize.X, (int)other.hitBoxSize.Y));
         }
-        public void loadSfx(SoundEffect sfx)
+        public void loadSfx(SoundEffect sfx) // generic sound effect loading method
         {
             this.sfx = sfx;
         }
