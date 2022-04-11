c7042cfd23acc1439d8a9eadb3249fccd1300f5b
yugi20015 <71729728+yugi20015@users.noreply.github.com>
2022-04-11 11:59:08 -0500

    comments

--- MamaSpider.cs
+++ MamaSpider.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
@@ -8,18 +8,20 @@ using System.Text;
 
 namespace Silent_Void
 {
-    class MamaSpider : Spider
+    class MamaSpider : Spider // subclass of spider
     {
+        // texture and constructor that takes in information from parent class
         public static new Texture2D texture;
         public MamaSpider(Vector2 pos, float rad) : base(texture, pos, rad)
         {
             base.size = new Vector2(72, 72);
             hitBoxSize = size - new Vector2(10, 10);
+            // manage speed and hp
             base.hp = 3;
-            base.speed = 7;
+            base.speed = 7; 
         }
 
-        public override void OnDeath()
+        public override void OnDeath() // spawn a batch of small spiders after death of big mama spider
         {
             for (int i = 0; i < 3; i++)
             {
@@ -27,7 +29,7 @@ namespace Silent_Void
             }
             base.OnDeath();
         }
-        public override void OnHit()
+        public override void OnHit()// spawn a spider after death of big mama spider
         {
             game.planes.Add(new Spider(this.pos, 0));
             base.OnHit();
