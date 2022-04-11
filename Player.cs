9d11be4a32cc997d6ceb02a0fae02274c7fd57fb
yugi20015 <71729728+yugi20015@users.noreply.github.com>
2022-04-11 12:00:14 -0500

    coments

--- Player.cs
+++ Player.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework;
@@ -13,8 +13,9 @@ using System.Diagnostics;
 namespace Silent_Void
 
 {
-    class Player : Entity
+    class Player : Entity // player is subclass of entity
     {
+        // needed instance fields for managing the player state like reload and speed
         int cooldown = 0;
         int reload = 10;
         public int points;
@@ -23,6 +24,7 @@ namespace Silent_Void
         public float hurtTransparency = 0f;
         public float iframe = 0;
         Vector2 defaultPos;
+        // player constructor - initialized a lot of base variables and class variables
         public Player(Texture2D tex, Vector2 pos, float rad)
         {
             base.colour = Color.White;
@@ -39,18 +41,20 @@ namespace Silent_Void
             vel = new Vector2(0, 0);
 
         }
-        public override void Update()
+        public override void Update() // update position of the player - overrides entity update
         {
+            // change directon based on mouse direction
             MouseState mouseState = Mouse.GetState();
 
             Vector2 mousePosition = new Vector2(mouseState.X, mouseState.Y);
 
             Vector2 dPos = this.pos - mousePosition;
 
-            rad = (float)Math.Atan2(dPos.Y, dPos.X);
+            rad = (float)Math.Atan2(dPos.Y, dPos.X); 
 
             //vel = -new Vector2((float)Math.Cos(rad), (float)Math.Sin(rad)) * 5;
 
+            // move left, right, up, down based WASD
             if (Keyboard.GetState().IsKeyDown(Keys.W))
 
             {
@@ -72,13 +76,13 @@ namespace Silent_Void
                 vel.Normalize();
                 vel *= speed;
             }
-
+            // increase velocity going right
             if (Keyboard.GetState().IsKeyDown(Keys.D))
 
             {
                 vel.X += acc;
             }
-
+            // rebounding the player 
             if (pos.X < 0)
             {
                 pos.X = 0;
@@ -99,20 +103,21 @@ namespace Silent_Void
                 pos.Y = 1080;
                 vel.Y = -vel.Y;
             }
-
+            // shoot 
             if (mouseState.LeftButton == ButtonState.Pressed && cooldown >= reload)
             {
                 Shoot(pos, rad, true);
                 cooldown = 0;
             }
             cooldown++;
-
+            // change how much its been hurt
             hurtTransparency -= 0.05f;
             if (hurtTransparency < 0)
             {
                 hurtTransparency = 0;
             }
 
+            // managing invincible state
             iframe--;
             if (iframe < 0)
             {
@@ -133,6 +138,7 @@ namespace Silent_Void
 
         public override void OnHit()
         {
+            // managing stuff once hit - calling base OnHit method
             if (iframe <= 0)
             {
                 hurtTransparency = 0.5f;
@@ -141,18 +147,18 @@ namespace Silent_Void
                 base.OnHit();
             }
         }
-
+        // reset the player's position, HP, and velocity presumably after each level
         public void reset()
         {
             base.hp = maxHp;
             pos = defaultPos;
             vel = new Vector2(0, 0);
         }
-
+        // shoot projectile with game bullet texture
         public void Shoot(Vector2 pos, float rad, bool friendly)
         {
             game.Add(new Projectile(game.bullet, pos, rad, friendly, 15));
-            sfx.Play();
+            sfx.Play(); // play sound effect of bullet
         }
     }
 }
