3182a0f17818061294ca92b952541a88311ab8f1
yugi20015 <71729728+yugi20015@users.noreply.github.com>
2022-04-11 12:01:11 -0500

    comment

--- Spider.cs
+++ Spider.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
@@ -8,17 +8,21 @@ using System.Text;
 
 namespace Silent_Void
 {
-    class Spider : Entity
+    class Spider : Entity // subclass of entity
     {
+        // needed instance fields for the class to function
         public static Texture2D texture;
         public int speed = 10;
         float acc = 0.2f;
         double deviance = 0;
+        // empty constructor that derives info from the other constructor
         public Spider(Vector2 pos, float rad) : this(texture, pos, rad)
         {
         }
+        // constructor that initializes various base and class variables
         public Spider(Texture2D tex, Vector2 pos, float rad)
         {
+            // managing position, hp, etc
             base.colour = Color.White;
             base.tex = tex;
             base.size = new Vector2(36, 36);
@@ -29,12 +33,15 @@ namespace Silent_Void
             friendly = false;
             isBullet = false;
         }
+        // update the spider position, speed, movement, etc
         public override void Update()
         {
+            // going towards the player - not too close though
             Vector2 target = player.pos - pos;
             rad = (float)(Math.Atan2(target.Y, target.X) + Math.PI);
 
             double ang = Math.Atan2(target.Y, target.X);
+            // making sure enemies dont bunch up together - deviance
             deviance += (Enemy.rnd.NextDouble() - 0.5) * Math.PI / 6;
             if (deviance > Math.PI / 4)
             {
@@ -48,12 +55,12 @@ namespace Silent_Void
 
             vel += new Vector2((float)Math.Cos(ang) * acc, (float)Math.Sin(ang) * acc);
 
-            if (vel.LengthSquared() > speed * speed)
+            if (vel.LengthSquared() > speed * speed) //make sure its not going too fast
             {
                 vel.Normalize();
                 vel *= speed;
             }
-
+            // making sure its within bounds, rebounding if needed
             if (pos.X < 0)
             {
                 pos.X = 0;
